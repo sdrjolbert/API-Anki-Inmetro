8ece9121773d226b057a9ac5b290eb549ed8c930
Jolbert Sodré <jolbert.sodre15@gmail.com>
2024-06-20 00:43:36 -0300

    Ajustar API

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,11 +1,15 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req) {}
 
-export async function POST(req) {
-  return NextResponse.json(
-    { ok: "OK" },
-    { message: "Conta criada!" },
-    { status: 201 }
-  );
+export async function POST(req = NextRequest()) {
+  const { username, email, password } = req.json();
+
+  return NextResponse.json({ username }, { email }, { password });
+
+  // return NextResponse.json(
+  //   { ok: "OK" },
+  //   { message: "Conta criada!" },
+  //   { status: 201 }
+  // );
 }
